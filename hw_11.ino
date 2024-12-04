b1e834784d76b25c4545fcf126bcdef60157776c
nikhil0119 <nikhilkhandelwal0119@gmail.com>
2024-12-04 17:27:05 -0500

    Update hw_11.ino

--- hw_11.ino
+++ hw_11.ino
@@ -1,11 +1,27 @@
-// Homework 11
+const int ledPins[] = {2, 3, 4, 5, 6, 7};
 
 void setup() {
-  // put your setup code here, to run once:
-
+  Serial.begin(9600); 
+  for (int i = 0; i < 6; i++) {
+    pinMode(ledPins[i], OUTPUT);
+    digitalWrite(ledPins[i], HIGH);
+  }
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  int seconds = (millis() / 1000) % 60;
+  int activeLEDs = 6 - (seconds / 10); // Number of LEDs to keep ON
 
+  for (int i = 0; i < 6; i++) {
+    if (i < activeLEDs) {
+      digitalWrite(ledPins[i], HIGH); // Keep LEDs ON
+    } else {
+      digitalWrite(ledPins[i], LOW); // Turn LEDs OFF
+    }
+  }
+
+  Serial.print("Elapsed Seconds: ");
+  Serial.println(seconds);
+
+  delay(500); 
 }
